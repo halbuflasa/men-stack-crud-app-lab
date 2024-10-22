f3893efd6c78f5d0859374e435e7f8ca4f1a554a
halbuflasa <albuflasahessa@gmail.com>
2024-10-22 07:03:10 +0300

    Read operation, add and show image are done

--- server.js
+++ server.js
@@ -2,16 +2,35 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const mongoose = require("mongoose");
-
+const multer = require("multer");
+const path = require("path");
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URI);
-mongoose.connection.on("connected",()=>{
+mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
 const Plant = require("./models/plant.js");
 app.use(express.urlencoded({ extended: false }));
+
+/////////////////////IMAGES///////////////////
+// Define the multer storage configuration
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); // Specify the uploads directory
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname)); // Add timestamp to filename
+  },
+});
+
+// Create an instance of multer with the defined storage
+const upload = multer({ storage });
+
+// Serve the uploads folder statically
+app.use("/uploads", express.static("uploads"));
+/////////////////////////////////////////////////////////
 
 app.get("/", async (req, res) => {
   res.render("index.ejs");
@@ -23,15 +42,26 @@ app.get("/plants", async (req, res) => {
   res.render("plants/index.ejs", { plants: allPlants });
 });
 
-app.get('/plants/new', async (req, res) =>{
+app.get('/plants/new', async (req, res) => {
   res.render("plants/new.ejs");
 });
-app.post('/plants', async (req, res)=>{
-  await Plant.create(req.body)
+
+app.get("/plants/:plantId", async (req, res) => {
+  const foundPlant = await Plant.findById(req.params.plantId);
+  res.render("plants/show.ejs", { plant: foundPlant });
+});
+
+// Use upload.single for handling the image upload in the form
+app.post("/plants", upload.single("image"), async (req, res) => {
+  const plantData = {
+    name: req.body.name,
+    description: req.body.description,
+    image: req.file ? req.file.filename : null, // Save the filename of the uploaded image
+  };
+  await Plant.create(plantData);
   res.redirect("/plants");
 });
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
-
